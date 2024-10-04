299dda1d0b37c572bef55fe4e44c891439f681fd
Nik <niklas@kizo-agency.com>
2024-10-04 13:29:12 +0200

    3_8 - Header CSS

--- src/lib/components/Layout/Header.svelte
+++ src/lib/components/Layout/Header.svelte
@@ -18,3 +18,20 @@
     </ul>
   </nav>
 </header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 4vw;
+  }
+  ul {
+    display: flex;
+    column-gap: 24px;
+  }
+
+  .logo {
+    height: 72px;
+  }
+</style>
