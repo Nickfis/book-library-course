0e64a52b64e06cb8343368636b3a43aa97ef6102
Nik <niklas@kizo-agency.com>
2024-10-05 18:02:46 +0200

    3_16 - Login form action

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import { Header } from "$components";
   import "./../app.css";
   import { invalidate } from "$app/navigation";
-  import { onMount } from "svelte";
 
   let { children, data } = $props();
   let { session, supabase, user } = $derived(data);
@@ -17,7 +16,8 @@
     return () => data.subscription.unsubscribe();
   });
 
-  $inspect(session)
+  $inspect(session);
+  $inspect(user);
 </script>
 
 <Header />
