8e184e8f70ff2f76b6a346d3aeaf4e95e6de814a
Nik <niklas@kizo-agency.com>
2024-10-06 13:13:23 +0200

    3_21 - Fetch books from supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,24 @@
   import { Header } from "$components";
   import "./../app.css";
   import { invalidate } from "$app/navigation";
-  import { setUserState } from "$components/state/user-state.svelte";
+  import { setUserState } from "$lib/state/user-state.svelte";
 
   let { children, data } = $props();
   let { session, supabase } = $derived(data);
 
-  let userState = setUserState({session: data.session, supabase: data.supabase, user: data.user});
+  let userState = setUserState({
+    session: data.session,
+    supabase: data.supabase,
+    user: data.user,
+  });
 
   $effect(() => {
     const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
-      userState.updateState({session: newSession, supabase, user: newSession?.user || null})
+      userState.updateState({
+        session: newSession,
+        supabase,
+        user: newSession?.user || null,
+      });
 
       if (newSession?.expires_at !== session?.expires_at) {
         invalidate("supabase:auth");
